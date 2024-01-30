42cf5ac9a15262b5e6222e152540a604a3a7812b
Pilag6 <pilag6@gmail.com>
2024-01-30 11:04:41 +0100

    Remove unused component and assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-import First from "@components/First.jsx";
 import "./App.css";
 
 function App() {
     return (
         <>
             <h1>Minimalistic React App with Vite</h1>
-            <First />
         </>
     );
 }
--- src/components/First.jsx
+++
@@ -1,9 +0,0 @@
-
-
-const First = () => {
-  return (
-    <div>First</div>
-  )
-}
-
-export default First
